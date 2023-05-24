e45b18418edbd1aa5fc5c08dc00bddbb5145830a
Jaspreet <46000676+singhjaspreetb@users.noreply.github.com>
2023-05-24 12:22:40 +0530

    vercel auth

--- src/main/java/com/involveininnovation/chat/config/CorsConfig.java
+++ src/main/java/com/involveininnovation/chat/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://singhjaspreetb.github.io/")
+                        .allowedOrigins("http://localhost:3000","https://singhjaspreetb.github.io/","https://techies-lodge-nine.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
